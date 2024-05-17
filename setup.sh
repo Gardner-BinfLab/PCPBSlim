2faf232204b6e8f47c7d6b2343a20d247de8c8fd
fienx <djchamp@gmail.com>
2024-05-17 15:01:59 +1200

    scripts

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 declare -A archives
 archives["results/compiledResults"]="results/compiledResults.tar.gz"
 archives["data/controls"]="data/controls.tar.gz"
+archives["scripts"]="scripts/scripts.tar.gz"
 
 # Loop through each directory-archive pair
 for dir in "${!archives[@]}"; do
